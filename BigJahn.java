5220853449dfb5947e2db9dbc23491bf45545c70
Kaden31504 <54899723+Kaden31504@users.noreply.github.com>
2019-09-10 08:57:31 -0500

    Add files via upload
    I did the middle part of the story

---
+++ BigJahn.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class BigJahn here.
+ * On Jah Breh
+ * @author (Kaden Marcell)
+ * @version (9-10-19)
+ */
+public class BigJahn
+{  
+   public static void main(String[] args){
+     System.out.println("Big Jahn is running from the Mega-Spiders and Clarence stops him and says");                                                                           
+     System.out.println("Do you know Joe?, BigJahn replies, Whos Joe.");
+     System.out.println("Clarance Shouts JOE MAMMA, but all the hold up caused an issue there was a spider wrapping BigJahn in a web");
+     System.out.println("He escapes the web and quickly runs into Costco to purchase the holy grail, The 23lb and 6oz bucket of Mac and Cheese");
+     System.out.println("He hops into his 2014 Ford Fiesta Model S and drives away to his club house to enjoy the mac and cheese");
+     System.out.println(args[0]);
+}
+}
