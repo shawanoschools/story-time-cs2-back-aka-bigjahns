bf464cb26cb542d1607373d95ae174fb8a9cea6e
liam-konge <54899912+liam-konge@users.noreply.github.com>
2019-09-10 08:57:33 -0500

    Add files via upload

--- BigJahn.java
+++ BigJahn.java
@@ -1,18 +1,30 @@
 
 /**
  * Write a description of class BigJahn here.
- * On Jah Breh
- * @author (Kaden Marcell)
- * @version (9-10-19)
+ *
+ * @author Liam Krueger
+ * @version 9/10/2019
  */
 public class BigJahn
-{  
-   public static void main(String[] args){
-     System.out.println("Big Jahn is running from the Mega-Spiders and Clarence stops him and says");                                                                           
-     System.out.println("Do you know Joe?, BigJahn replies, Whos Joe.");
-     System.out.println("Clarance Shouts JOE MAMMA, but all the hold up caused an issue there was a spider wrapping BigJahn in a web");
-     System.out.println("He escapes the web and quickly runs into Costco to purchase the holy grail, The 23lb and 6oz bucket of Mac and Cheese");
-     System.out.println("He hops into his 2014 Ford Fiesta Model S and drives away to his club house to enjoy the mac and cheese");
-     System.out.println(args[0]);
-}
+{
+    public static void main(String[] args){
+        System.out.println("Once upon a time...");
+        System.out.println("There was a big man named Big Jahn");
+        System.out.println("He came from the ruling Jahn family,");
+        System.out.println("and his parents bestowed him with the traditional family name Big");
+        System.out.println("One day, Big Jahn went out to the snowy mountains to hunt for rabbits");
+        System.out.println("He went about the snowy mountain forest with his beautiful hunting musket that had been given to him by his grandmother");
+        System.out.println("Thought Big Jahn,'That looks like a person.'");
+        System.out.println("Since Big Jahn's interactions with people throughout his life time had not been positive,");
+        System.out.println("Big Jahn decided to run away");
+        System.out.println("As Big Jahn ran, he could hear the mocking cries of the rabbits, taunting him for his cowardice.");
+        System.out.println("Big Jahn decided that he would not tolerate the rabbits, so he summoned up the memory of the man he admired most,");
+        System.out.println("Jahn McCain, and his vegetable friends. Filled with strength, Big Jahn marched towards what he thought has a person.");
+        System.out.println("What Big Jahn thought to be a person was indeed a person, though he looked a bit like a zucchini with an evil soul.");
+        System.out.println("The zucchini-looking man introduced himself as Clarence.");
+        System.out.println("Clarence whisphered into Big Jahn's ear, 'Hark... my mother is nigh");
+        System.out.println("Suddenly, a Mega-Spider burst out of the trees!");
+        System.out.println("Frightened, Big Jahn dropped his musket and started running down the mountain, with the Mega-Spider in close pursuit");
+        System.out.println("He took refuge in a swamp, where he somehow happened upon Clarence");
+    }
 }
