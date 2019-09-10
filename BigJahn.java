7bc2eedca5b587e7221289fa12192e1a5c41dfb0
Taylor-25 <54899165+Taylor-25@users.noreply.github.com>
2019-09-10 09:02:51 -0500

    Add files via upload

--- BigJahn.java
+++ BigJahn.java
@@ -2,29 +2,19 @@
 /**
  * Write a description of class BigJahn here.
  *
- * @author Liam Krueger
- * @version 9/10/2019
+ * @Taylor Pederson
+ * @version ((9/10/2k19)
  */
 public class BigJahn
 {
-    public static void main(String[] args){
-        System.out.println("Once upon a time...");
-        System.out.println("There was a big man named Big Jahn");
-        System.out.println("He came from the ruling Jahn family,");
-        System.out.println("and his parents bestowed him with the traditional family name Big");
-        System.out.println("One day, Big Jahn went out to the snowy mountains to hunt for rabbits");
-        System.out.println("He went about the snowy mountain forest with his beautiful hunting musket that had been given to him by his grandmother");
-        System.out.println("Thought Big Jahn,'That looks like a person.'");
-        System.out.println("Since Big Jahn's interactions with people throughout his life time had not been positive,");
-        System.out.println("Big Jahn decided to run away");
-        System.out.println("As Big Jahn ran, he could hear the mocking cries of the rabbits, taunting him for his cowardice.");
-        System.out.println("Big Jahn decided that he would not tolerate the rabbits, so he summoned up the memory of the man he admired most,");
-        System.out.println("Jahn McCain, and his vegetable friends. Filled with strength, Big Jahn marched towards what he thought has a person.");
-        System.out.println("What Big Jahn thought to be a person was indeed a person, though he looked a bit like a zucchini with an evil soul.");
-        System.out.println("The zucchini-looking man introduced himself as Clarence.");
-        System.out.println("Clarence whisphered into Big Jahn's ear, 'Hark... my mother is nigh");
-        System.out.println("Suddenly, a Mega-Spider burst out of the trees!");
-        System.out.println("Frightened, Big Jahn dropped his musket and started running down the mountain, with the Mega-Spider in close pursuit");
-        System.out.println("He took refuge in a swamp, where he somehow happened upon Clarence");
-    }
+  public static void main(String[] args){
+       System.out.println("Big Jahn thought 'Hmmm, How did I get here?'");
+       System.out.println("Clarence had died from spider wounds.");
+       System.out.println("Big Jahn was on his last few Mac and Cheese packets.");
+       System.out.println("Out in the middle of the fairy field west of the large think box Big Jahn sat.");
+       System.out.println("He didn't know how to get back home. He just started to walk.");
+       System.out.println("Big Jahn had a long journey to the store and just wanted to go home.");
+       System.out.println("He walked down tree lane, passed the zoom zoom track, and turned down blue 52..");
+       System.out.println("And finally Mac and Cheese and milk in hand he arrived back home safe and sound."); 
+}
 }
